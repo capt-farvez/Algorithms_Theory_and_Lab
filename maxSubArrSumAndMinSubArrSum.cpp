4e4f5ce7c9ff4f900a8f357e8431c1286bab820c
Anowar Hossen Farvez <farvez165@gmail.com>
2022-09-10 02:11:45 +0600

    Updated Documentation Format

--- maxSubArrSumAndMinSubArrSum.cpp
+++ maxSubArrSumAndMinSubArrSum.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int maxSubArraySum(int a[], int size)
 {
     ll max_so_far = INT_MIN, max_ending_here = 0;
-  
+
     for (int i = 0; i < size; i++)
     {
         max_ending_here = max_ending_here + a[i];
         if (max_so_far < max_ending_here)
             max_so_far = max_ending_here;
-  
+
         if (max_ending_here < 0)
             max_ending_here = 0;
     }
@@ -21,39 +21,43 @@ int smallestSumSubarr(int arr[], int n)
 {
     int min_ending_here = INT_MAX;
     int min_so_far = INT_MAX;
-     
-    for (int i=0; i<n; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        if (min_ending_here > 0){
+        if (min_ending_here > 0)
+        {
             min_ending_here = arr[i];
         }
-           
-        else{
+
+        else
+        {
             min_ending_here += arr[i];
         }
-        min_so_far = min(min_so_far, min_ending_here);           
+        min_so_far = min(min_so_far, min_ending_here);
     }
-     
+
     return min_so_far;
 }
 
-int main() {
+int main()
+{
     ll t;
-    cin>>t;
-    while (t--){
+    cin >> t;
+    while (t--)
+    {
         int size;
-        cin>>size;
+        cin >> size;
         int a[size];
-        for(int i=0; i<size; i++){
+        for (int i = 0; i < size; i++)
+        {
             int x;
-            cin>>x;
-            a[i]=x;
-
+            cin >> x;
+            a[i] = x;
         }
-        //int n = sizeof(a)/sizeof(a[0]);
+        // int n = sizeof(a)/sizeof(a[0]);
         int max_sum = maxSubArraySum(a, size);
-        int smallest_sum= smallestSumSubarr(a, size);
-        cout << smallest_sum<< " " << max_sum<<endl;
+        int smallest_sum = smallestSumSubarr(a, size);
+        cout << smallest_sum << " " << max_sum << endl;
     }
-	return 0;
+    return 0;
 }
--- maximumSubArraySum.cpp
+++ maximumSubArraySum.cpp
@@ -1,53 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int CrossSum(int arr[], int low, int mid, int high)
 {
-    int sum =0,i;
+    int sum = 0, i;
     int lsum = INT_MIN;
     int rsum = INT_MIN;
 
-    for (i=mid;i>=low;i--)
+    for (i = mid; i >= low; i--)
     {
-        sum+=arr[i];
-        if (lsum<sum)
-            lsum=sum;
+        sum += arr[i];
+        if (lsum < sum)
+            lsum = sum;
     }
-    sum=0;
-    for (i=mid+1;i<=high;i++)
+    sum = 0;
+    for (i = mid + 1; i <= high; i++)
     {
-        sum+=arr[i];
-        if (rsum<sum)
-         rsum=sum;
+        sum += arr[i];
+        if (rsum < sum)
+            rsum = sum;
     }
-    return lsum+rsum;
+    return lsum + rsum;
 }
 
 int mss(int arr[], int low, int high)
 {
-    if (low==high)
+    if (low == high)
         return arr[low];
-    int mid = (low+high)/2;
-    int rsum = mss(arr, mid+1, high);
-    int lsum = mss(arr,low,mid);
+    int mid = (low + high) / 2;
+    int rsum = mss(arr, mid + 1, high);
+    int lsum = mss(arr, low, mid);
     int cross = CrossSum(arr, low, mid, high);
     return max(max(lsum, rsum), cross);
-
 }
 
 int main()
 {
     int size;
-        cin>>size;
-        int arr[size];
-        for(int i=0; i<size; i++){
-            int x;
-            cin>>x;
-            arr[i]=x;
-
-        }
-    int result = mss(arr, 0, size-1);
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        int x;
+        cin >> x;
+        arr[i] = x;
+    }
+    int result = mss(arr, 0, size - 1);
     printf("Maximum subarray sum is : %d", result);
     return 0;
-
 }
